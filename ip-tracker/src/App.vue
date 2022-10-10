3151dc5c02ae2a085e3e1732c52e41b692182a6e
RayzCoco <nicowhong@gmail.com>
2022-10-11 01:25:19 +0800

    added search bar, tracker indicator and skeleton of page.

--- ip-tracker/src/App.vue
+++ ip-tracker/src/App.vue
@@ -1,53 +1,49 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <header class="pattern-bg">
+    <div class="container mx-auto pt-4 pb-16">
+      <h1 class="font-bold text-2xl text-white text-center pb-4">IP Address Tracker</h1>
+      <div class="flex justify-center pb-6">
+        <input placeholder="Search for any IP address or domain" class="rounded-l-xl px-6 w-2/6 text-sm" type="text">
+        <button class="bg-black p-4 flex justity-center rounded-r-xl">
+          <img src="./assets/icon-arrow.svg" alt="click here to search!" />
+        </button>
+      </div>
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main class="relative">
+    <div class="grid grid-cols-4 w-3/5 rounded-lg py-6 px-5 bg-white drop-shadow-lg absolute -top-12 left-1/2 -translate-x-2/4">
+      <div class="border-r">
+        <h2>ip address</h2>
+        <div class="font-bold">{{ count }}</div>
+      </div>
+      <div class="border-r px-4">
+        <h2>location</h2>
+        <div class="font-bold">area name</div>
+      </div>
+      <div class="border-r">
+        <h2>timezone</h2>
+        <div class="font-bold">utc</div>
+      </div>
+      <div>
+        <h2>isp</h2>
+        <div class="font-bold">unifi</div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="font-bold">Map Here</div>
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {
+  setup() {
+    const count = 0
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    return {
+      count
+    }
   }
 }
-</style>
+</script>
